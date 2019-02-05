46b89fa2feff5e672217d0b375c4c73c814de28a
Calling6850 <ruben.otter3@gmail.com>
2019-02-05 20:04:33 +0100

    BasisPt1

---
+++ AssemblyTycoon/Assets/Scripts/CardboardBox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardboardBox : MonoBehaviour {
+	public float move = -1f;
+	public static float CardboardSpeed = 5f;
+	public static float CardboardSpeedUpgrade = 5f;
+	public static float CardboardValue = 1;
+	public static float CardboardAmount = 1;
+	public Transform CBox;
+	private Rigidbody2D rb;
+	void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+		CardboardSpeed = CardboardSpeedUpgrade;
+
+		rb.velocity = new Vector2(move * CardboardSpeed, rb.velocity.y);
+
+		if (CBox.position.x < -5)
+			CardboardSpeed = 0f;
+		if (CBox.position.y < -5)
+		{
+			CBox.position = new Vector3(12, -2, 0);
+			CardboardSpeed = CardboardSpeedUpgrade;
+			Money.MoneyAmount += CardboardValue;
+		}
+    }
+}
---
+++ AssemblyTycoon/Assets/Scripts/Money.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money : MonoBehaviour {
+	public static float MoneyAmount = 0;
+	Text money;
+
+    void Start()
+    {
+		money = GetComponent<Text>();
+    }
+
+    void FixedUpdate()
+    {
+		money.text = "Money: $" + MoneyAmount;
+    }
+}
